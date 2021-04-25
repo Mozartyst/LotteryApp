03895b463c4909cbf37ddabef1b9a79aacf58145
mozartyst <M810107t>
2021-04-25 11:59:59 +0100

    Changed: Fixed AllDrawsGraph for Draws without BonusBalls

--- src/main/java/printers/AllDrawsGraph.java
+++ src/main/java/printers/AllDrawsGraph.java
@@ -9,14 +9,7 @@ public class AllDrawsGraph {
     public void print(ArrayList<OneDraw> lotteryNumbers, Properties properties) {
         for (OneDraw weekDraw : lotteryNumbers) {
             for (int i = 1; i <= Integer.parseInt(properties.getProperty("range")); i++) {
-                if (weekDraw.getBonusBalls() == null) {
-                    if (weekDraw.getDrawNumbers().contains(i)) {
-                        System.out.print("\u001B[42m" + "\u001B[30m" + String.format("%02d", i) + "\u001B[0m");
-                        System.out.print("\u001B[33m" + "|" + "\u001B[0m");
-                    } else {
-                        System.out.print("\u001B[33m" + "  |" + "\u001B[0m");
-                    }
-                } else {
+                if (Boolean.parseBoolean(properties.getProperty("bonusBalls"))) {
                     if (weekDraw.getDrawNumbers().contains(i) || weekDraw.getBonusBalls().contains(i)) {
                         if (weekDraw.getBonusBalls().contains(i)) {
                             System.out.print("\u001B[41m" + "\u001B[30m" + String.format("%02d", i) + "\u001B[0m");
@@ -28,6 +21,14 @@ public class AllDrawsGraph {
                     } else {
                         System.out.print("\u001B[33m" + "  |" + "\u001B[0m");
                     }
+                } else {
+                    if (weekDraw.getDrawNumbers().contains(i)) {
+                        System.out.print("\u001B[42m" + "\u001B[30m" + String.format("%02d", i) + "\u001B[0m");
+                        System.out.print("\u001B[33m" + "|" + "\u001B[0m");
+                    } else {
+                        System.out.print("\u001B[33m" + "  |" + "\u001B[0m");
+                    }
+
                 }
             }
             System.out.println();
